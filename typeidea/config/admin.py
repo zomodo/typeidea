8727200208f3b5a0e13099799ab004e1599cbd8d
zhangmengdi <986001564@qq.com>
2019-12-25 10:33:52 +0800

    修改了comment下的admin，配置后台 修改了config下的admin，配置后台

--- typeidea/config/admin.py
+++ typeidea/config/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Link,SlideBar
 
 # Register your models here.
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title','href','status','weight','owner','created_time')
+    fields = ('title','href','status','weight')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner=request.user
+        return super(LinkAdmin,self).save_model(request,obj,form,change)
+
+@admin.register(SlideBar)
+class SlideBarAdmin(admin.ModelAdmin):
+    list_display = ('title','display_type','content','status','owner','created_time')
+    fields = ('title','display_type','content','status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner=request.user
+        return super(SlideBarAdmin,self).save_model(request,obj,form,change)
+
