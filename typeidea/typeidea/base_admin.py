0f404762e0d1d8b7909a15bd37736f3024d1d319
zhangmengdi <986001564@qq.com>
2020-02-23 15:46:42 +0800

    base_admin中修改了一些内容，后台中判断用户，如果是超级用户就显示所有数据，否则只显示当前用户的数据

--- typeidea/typeidea/base_admin.py
+++ typeidea/typeidea/base_admin.py
@@ -4,14 +4,18 @@
 from django.contrib import admin
 class BaseOwnerAdmin(admin.ModelAdmin):
     """
-    1.用来自动补充文章，分类，标签，侧边栏，友链这些models的owner字段
-    2.用来针对queryset过滤当前用户的数据
+    1.用来针对queryset过滤当前用户的数据，这里设置为如果是超级用户就展示所有数据，否则只展示当前用户的数据
+    2.用来自动补充文章，分类，标签，侧边栏，友链这些models的owner字段
     """
     exclude = ('owner',)
 
     def get_queryset(self, request):
         qs=super(BaseOwnerAdmin, self).get_queryset(request)
-        return qs.filter(owner=request.user)
+        # 先对用户进行判断，如果是超级用户就展示所有数据，如果不是，则只展示当前用户的数据
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(owner=request.user)
 
     def save_model(self, request, obj, form, change):
         obj.owner=request.user
