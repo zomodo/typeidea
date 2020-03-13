cf6175fe9c0b3ac19a5213d66dd02912bed31115
zhangmengdi <986001564@qq.com>
2020-03-13 14:42:06 +0800

    创建部署相关的文件配置

---
+++ typeidea/build/lib/settings/develop.py
@@ -0,0 +1,69 @@
+# Author : zmd
+# Date : 2019/12/6 17:09
+# Desc :配置开发环境设置
+
+from .base import *
+
+DEBUG=True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# 配置MySQL数据库
+# DATABASES = {
+#     'default':{
+#         'ENGINE':'django.db.backends.mysql',
+#         'NAME':'typeidea_db',
+#         'USER':'root',
+#         'PASSWORD':'123456',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306',
+#         'CONN_MAX_AGE':5*60,
+#         'OPTIONS':{'charset':'utf8mb4'},
+#     },
+# }
+
+# 之所以在develop.py中增加，因为该工具只能在开发和测试阶段使用
+# 并且django-debug-toolbar只有在DEBUG=True时才会生效
+
+INSTALLED_APPS += [
+    'debug_toolbar',    # develop设置中引入django-debug-toolbar,用于做性能排查
+    'pympler',          # 引入内存占用分析
+    # 'debug_toolbar_line_profiler',      # 引入行级性能分析插件
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',  # django-debug-toolbar中间件
+]
+
+INTERNAL_IPS = ['127.0.0.1']
+
+# 配置django-debug-toolbar
+DEBUG_TOOLBAR_PANELS = [
+
+    # 默认的panel
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+
+    # 第三方panel
+    # 'djdt_flamegraph.FlamegraphPanel',  # djdt_flamegraph火焰图，windows中可能不兼容
+    'pympler.panels.MemoryPanel',         # 内存占用分析
+    # 'debug_toolbar_line_profiler.panel.ProfilingPanel',     # 行级性能分析
+]
+# 如果上方的debug_toolbar配置展示不出来，可以修改JQUERY_URL为国内的CDN地址
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL':'https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js',
+}
---
+++ typeidea/setup.py
@@ -0,0 +1,75 @@
+# 配置项目的setup.py
+from setuptools import setup,find_packages
+
+setup(
+    name='typeidea',
+    version='0.1',
+    description='Blog System base on Django',
+    author='zomodo',
+    author_email='986001564@qq.com',
+    url='#',
+    license='MIT',
+    packages=find_packages('typeidea'),
+    package_dir={'':'typeidea'},
+
+    # 方法一：打包数据文件，需按照目录层级匹配,package_data指除了.py文件以外还需要打包哪些
+    # package_data={'':[
+    #     'themes/*/*/*/*',
+    # ]},
+
+    # 方法二：打包数据文件，配合MANIFEST.in文件，使用较多,两个方法可以共存
+    include_package_data=True,
+
+    install_requires=[
+        'django==1.11.26',
+        'dj-pagination==2.4.0',
+        'supervisor==4.1.0',
+        'xadmin==0.6.1',
+        'mysqlclient==1.4.6',
+        'django-ckeditor==5.4.0',
+        'django-rest-framework==0.1.0',
+        'django-redis==4.9.0',
+        'django-autocomplete-light==3.2.10',
+        'mistune==0.8.4',
+        'Pillow==5.1.0',
+        'coreapi==2.3.3',
+        'django-debug-toolbar==1.9.1',
+
+    ],
+    extras_require={
+        'ipython':['ipython==6.2.1'],
+    },
+    scripts=[
+        './manage.py',
+    ],
+    entry_points={
+        'console_scripts':[
+            'typeidea_manage=manage:main',
+        ]
+    },
+    classifiers=[
+        # 软件成熟度如何，一般有下面几项
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # 指明项目的受众
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        # 选择项目许可证（License）
+        'License :: OSI Approved :: MIT License',
+
+        # 指定项目需要使用的python版本
+        'Programming Language :: Python :: 3.6',
+
+    ],
+
+)
+
+"""
+配置好之后就打包
+python setup.py sdist:打包之后以.tar.gz结尾
+python setup.py bdist_wheel:打包是wheel格式的，以whl结尾，优先使用wheel包
+"""
